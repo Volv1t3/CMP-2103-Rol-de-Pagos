0f7dfa6bd9b366cc84965c28879ed37a032e50fc
Santiago Arellano <santiagof2204@hotmai.com>
2024-04-24 23:51:11 -0500

    [Development Branch] [Code Changes] [Santiago Arellano]
    The present commit includes the general definition for the Base Employee class, it implements most methods, including string, compare, compareTo, and opens the door for further work which includes salary, salary attributes and calculations and serialization.
    
    The class is fully documented
    
    Included Xml Serializable Classes

---
+++ src/main/java/EmployeeAbstraction/EmployeeListWrapper.java
@@ -0,0 +1,84 @@
+package EmployeeAbstraction;
+/*======================================================================================================================
+ ?                                                     ABOUT
+ * @author         :  Carlos Villafuerte, Santiago Arellano
+ * @repo           :  CMP2103 - Rol De Pagos
+ * @description    :  Definicion e Implementacion de la clase EmployeListWrapper
+ =====================================================================================================================*/
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
+
+@XmlRootElement(name = "Empleados")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class EmployeeListWrapper {
+
+    /**
+     * Lista de Empleados a Registrar.
+     */
+    @XmlElement(name = "Empleado")
+    private List<Employee> m_employees;
+
+    //? Main No Args constructor requerido por JAXB
+    @SuppressWarnings("unused")
+    public EmployeeListWrapper() {}
+
+    /**
+     * Constructor sin parametros.
+     * Este constructor es requerido por JAXB (Java Architecture for XML Binding) y
+     * sirve para la deserializacion de XML y el objeto EmployeeListWrapper.
+     */
+
+    @SuppressWarnings("unused")
+    public EmployeeListWrapper(List<Employee> e_employees) {
+        this.m_employees = e_employees;
+    }
+
+    /**
+     * Metodo para obtener la lista de empleados
+     *<br><br>
+     * Este metodo devuelve una lista de objetos del tipo 'Employee'. No recibe ningun
+     * parametro como entrada.
+     *<br><br>
+     * El metodo `getM_employees` se usa principalmente para obtener la lista
+     * de empleados que esta siendo manejada dentro de la instancia de la clase
+     * 'EmployeeListWrapper' en la que se llama.
+     *<br>
+     *<br>
+     * Ejemplo de uso tipico:
+     * <br><br>
+     *
+     * <code>
+     * EmployeeListWrapper wrapper = new EmployeeListWrapper(someEmployeeList); <br>
+     * List<Employee> employeeList = wrapper.getM_employees() </code>
+     *
+     * @return <code> List< Employee > </code> Una lista de objetos 'Employee'
+     */
+    @SuppressWarnings("unused")
+    public List<Employee> getM_employees() {
+        return m_employees;
+    }
+
+    /**
+     * Establece la lista de empleados.
+     *<br><br>
+     * Este método recibe como entrada una lista de objetos del tipo 'Employee' y los
+     * asigna a la lista interna 'm_employees'.
+     * Ejemplo de uso tipico:
+     *<br><br>
+     *
+     * <code>
+     * EmployeeListWrapper wrapper = new EmployeeListWrapper(); <br>
+     * wrapper.setM_employees(someEmployeeList); </code>
+     *<br><br>
+     * @param e_employees <code> List< Employee > </code> Una lista de objetos 'Employee'
+     */
+    @SuppressWarnings("unused")
+    public void setM_employees(List<Employee> e_employees) {
+        this.m_employees = e_employees;
+    }
+
+}
--- src/main/java/EmployeeAbstraction/Manager.java
+++ src/main/java/EmployeeAbstraction/Manager.java
@@ -7,6 +7,171 @@ package EmployeeAbstraction;
  * @description    :  Definicion e Implementacion de la Clase Derivada Manager
  *===================================================================================================================**/
 
+import javax.naming.directory.InvalidAttributeValueException;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+import java.io.Serializable;
+import java.util.Objects;
 
-public class Manager extends Employee{
+@XmlRootElement(name = "Manager")
+@XmlType(propOrder = {"m_TituloNivelManager, m_ComisionManager"})
+public class Manager extends Employee implements Serializable {
+
+    /**
+     * String representativa del titulo de nivel superior del Manager a Registrar.
+     */
+
+    private String m_TituloNivelManager;
+    /**
+     * Float representativa de la comision del Manager a Registrar. Utiliza un valor estandar, 15%.
+     */
+
+    private Float m_ComisionManager;
+
+    //? Constructor Simple sin Argumentos Para JAXB
+    /**
+     * Constructor de la clase Manager.
+     * Este constructor no toma argumentos y se utiliza principalmente para la inicializacion de objetos con JAXB.
+     * Llama explícitamente al constructor de la clase base Employee.
+     */
+    public Manager(){super();}
+
+    //? Constructor Con Cinco Argumentos para JAXB
+    /**
+     * Este es un constructor de la clase Manager con cinco argumentos especificos, utilizado principalmente para JAXB.
+     * Este constructor llama explicitamente al constructor de la superclase Employee con algunos de los argumentos proporcionados.
+     *
+     * @param e_TituloNivelManager Un {@code String} que representa el titulo de nivel superior del Manager a registrar. No puede estar vacio.
+     * @param e_ComisionManager Un {@code float} que representa la comision del Manager a registrar. El valor de comision debe estar en el intervalo abierto de (0,1).
+     * @param e_NombreEmpleado Un {@code String} que representa el nombre del empleado.
+     * @param e_ApellidoEmpleado Un {@code String} que representa el apellido del empleado.
+     * @param e_CodigoEmpleado Un {@code int} que representa el código único del empleado.
+     * @param e_FechaContratacionEmpleado Un {@code long} que representa la fecha en que se contrató al empleado.
+     * @param e_sueldoEmpleado Un {@code float} que representa el salario del empleado. El valor del salario debe estar en el intervalo cerrado de [800,3500].
+     *
+     * @throws InvalidAttributeValueException si alguna de las entradas no cumple con los criterios especificos para cada parametro.
+     */
+    public Manager(String e_TituloNivelManager, float e_ComisionManager, String e_NombreEmpleado, String e_ApellidoEmpleado,
+                   int e_CodigoEmpleado, long e_FechaContratacionEmpleado, float e_sueldoEmpleado) throws InvalidAttributeValueException
+    {
+        super(e_NombreEmpleado, e_ApellidoEmpleado, e_CodigoEmpleado, e_FechaContratacionEmpleado, e_sueldoEmpleado);
+        this.setM_TituloNivelManager(e_TituloNivelManager);
+        this.setM_ComisionManager(e_ComisionManager);
+    }
+
+
+
+    //? Getters y Setters en Base A Beans Syntaxis para JAXB
+    /**
+     * Este metodo ajusta el Titulo del Nivel de Gerencia para el objeto Manager.
+     * Este metodo espera recibir un String no vacio que sera utilizado para actualizar el Titulo de Nivel de Gerencia de este objeto Manager.
+     *
+     * @param e_TituloNivelManager Un {@code String} que representa el Titulo de Nivel de Gerencia para el Manager a registrar. No puede estar vacio.
+     * @return Un {@code boolean} que indica si el Titulo de Nivel de Gerencia fue configurado correctamente.
+     * @throws InvalidAttributeValueException si el String proporcionado esta vacio.
+     *
+     */
+    public boolean setM_TituloNivelManager(String e_TituloNivelManager) throws InvalidAttributeValueException {
+
+        if (!(e_TituloNivelManager.isEmpty()))
+        {
+            this.m_TituloNivelManager = e_TituloNivelManager;
+            return true;
+        }
+        else {throw Employee.InvalidAttributeGenerator("setM_TituloNivelManager", e_TituloNivelManager);}
+    }
+    /**
+     * Esta funcion devuelve el Titulo de Nivel de Gerencia del objeto Manager.
+     * <br><br>
+     * Invoca al constructor de Employee subyacente para obtener la informacion del objeto Manager instanciado.
+     * No se necesita ningun aporte para esta funcion, ya que solo se trata de un getter que devuelve un valor preexistente.
+     *<br><br>
+     * @return Un {@code String} que representa el titulo de nivel superior del Manager registrado.
+     */
+    @XmlElement(name = "TituloNivelManager")
+    public String getM_TituloNivelManager() {return this.m_TituloNivelManager;}
+
+    /**
+     * Este metodo establece la comision del Manager.<br>
+     * Este metodo invoca al constructor subyacente de la clase Employee para establecer la comision<br>
+     * del objeto Manager si el valor proporcionado esta en el rango aceptable de (0,5000].<br>
+     *
+     * @param e_ComisionManager Un {@code float} que representa la comision del Manager a registrar. El valor de la comision debe estar en el intervalo abierto de (0,1).
+     * @return Un {@code boolean} que indica si la comision fue configurada correctamente.
+     * @throws InvalidAttributeValueException si el valor proporcionado no esta en el intervalo abierto de (0,5000].
+     */
+    public boolean setM_ComisionManager(float e_ComisionManager) throws InvalidAttributeValueException {
+
+        if (e_ComisionManager > 0 && e_ComisionManager <= 5000)
+        {
+            this.m_ComisionManager = e_ComisionManager;
+            return true;
+        }
+        else {throw Employee.InvalidAttributeGenerator("setM_ComisionManager", String.valueOf(e_ComisionManager));}
+    }
+    /**
+     * Esta funcion devuelve la comision del objeto Manager.
+     * <br><br>
+     * Esta funcion no requiere ningun aporte, ya que solo se trata de un getter que devuelve un valor preexistente.
+     * Internamente, esta llamando al constructor de la clase Employee para obtener la informacion del objeto Manager instanciado.
+     * Puede lanzar InvalidAttributeValueException si el atributo m_ComisionManager de Manager contiene un valor no valido.
+     *<br><br>
+     * @return Un {@code float} que representa la comision del Manager registrado.
+     * @throws InvalidAttributeValueException si m_ComisionManager tiene un valor no valido.
+     */
+    @XmlElement(name = "ComisionManager")
+    public Float getM_ComisionManager() {return this.m_ComisionManager;}
+
+
+    //? Implementacion de la clase Comparable con el Sueldo y la Comision
+
+    /**
+     * Este metodo realiza una comparacion sobre el objeto 'Manager' en base al salario total (sueldo y comision).
+     * Se realiza una llamada al método compareTo de la clase Float para llevar a cabo la comparación.
+     *  <br><br>
+     * @param other El objeto 'Employee' al que se le hara la comparacion. Este no puede ser nulo.
+     * @return Un valor entero que indica el resultado de la comparacion. Si el valor es
+     *         0, indica que ambas instancias son iguales.
+     *         Si el valor es menor que 0, indica que 'this' es menor que 'other'.
+     *         Si el valor es mayor que 0, indica que 'this' es mayor que 'other'.
+     * @throws NullPointerException si el parametro 'other' es nulo.
+     */
+    @Override
+    public int compareTo(Employee other) {
+
+        //? Paso Base revision de null values en other
+        if (other == null) {throw new NullPointerException("Error Code 0x0001 - [Raised] Invalid Argument passed into compareTo method, second arg. is null");}
+        //! Paso inductivo: usamos el compareTo robusto de Float, evitamos implementacion propia
+        return Objects.compare(this.getM_sueldoEmployee() + this.getM_ComisionManager(),
+                other.getM_sueldoEmployee() + this.getM_ComisionManager(), Float::compareTo);
+    }
+
+    //? Implementacion del metodo toString()
+
+    /**
+     * Este metodo sobreescribe el metodo toString de la superclase Empleado y devuelve una representacion de String de un Manager.
+     * <br><br>
+     * Los atributos del Manager se presentan en el siguiente orden:<br>
+     * - Titulo de Nivel de Gerencia del Manager<br>
+     * - Bonificacion del Manager<br>
+     * - Atributos de la superclase Empleado:<br>
+     *   - Nombre del empleado<br>
+     *   - Apellido del empleado<br>
+     *   - ID del empleado<br>
+     *   - Fecha de contratacion<br>
+     *   - Salario del empleado<br>
+     *   - Desglose del salario<br>
+     * <br>
+     * Cada atributo se muestra con su nombre y su valor, separados por dos puntos (:).<br>
+     * Los atributos están separados por caracteres de tabulación (\t).<br>
+     * @return Una cadena formateada que representa el estado actual del Manager.
+     */
+    @Override
+    public String toString() {
+
+        String newManagerRep = String.format("['Titulo Manager: %s;\t'Bono Manager': %s;\t",
+                this.getM_TituloNivelManager(), String.valueOf(this.getM_ComisionManager()));
+        return newManagerRep + super.toString().replace('[', ' ');
+    }
 }
